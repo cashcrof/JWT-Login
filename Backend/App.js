c4afdff5e3167b686e97c87e9ca1647c1ef39ea4
cashcrof <ceilinatrix@gmail.com>
2023-04-11 15:37:38 -0700

    new

--- Backend/App.js
+++ Backend/App.js
@@ -14,6 +14,7 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors());
 app.use(upload.array());
+app.use(express.static('public'))
 
 app.get("/*", (req, res) => {
   res.send("Hello World")
