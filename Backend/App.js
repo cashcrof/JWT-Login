3b2145030bf84f7f1c29866baca0efe4161a7e3a
cashcrof <ceilinatrix@gmail.com>
2023-04-11 14:56:12 -0700

    new

--- Backend/App.js
+++ Backend/App.js
@@ -15,7 +15,9 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(cors());
 app.use(upload.array());
 
-app.get
+app.get("*", (req, res) => {
+  res.send("Hello World")
+})
 
 app.post("/api/login", async (req, res) => {
   const {email, password: plaintextPassword} = req.body;
